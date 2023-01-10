fa25d8cdae309d90a1c17542310e3545a71113d3
exaharious <arifemrekarabacak@gmil.com>
2023-01-10 22:48:32 +0300

    10.01.2023

--- src/test/java/day09/C02_WindowHandle3.java
+++ src/test/java/day09/C02_WindowHandle3.java
@@ -1,5 +1,43 @@
 package day09;
 
-public class C02_WindowHandle3 {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WindowType;
+import utilities.TestBase;
+
+public class C02_WindowHandle3 extends TestBase {
+
+    @Test
+    public void newWindowHandleTest(){
+
+        // 1. TechPro education sayfasinin titleinin "Techpro Education" metnini icerdigini test edin
+        driver.get("https://www.techproeducation.com");
+        String techProHandle=driver.getWindowHandle();
+        Assert.assertTrue(driver.getTitle().contains("Techpro Education"));
+
+        // 2. Yeni bir pencerede Amazon sayfasini acip  titleinin "Amazon" metnini icerdigini test edin
+        waitFor(3);
+        driver.switchTo().newWindow(WindowType.WINDOW);//1. yeni pencere olusturur. 2. Oraya gecis yapar
+        driver.get("http://amazon.com");
+        Assert.assertTrue(driver.getTitle().contains("Amazon"));
+
+        // 3. Yeni bir pencerede Linkedin sayfasini acip titleinin "Linkedin" metnini icerdigini test edin
+        waitFor(3);
+        driver.switchTo().newWindow(WindowType.WINDOW);//1. yeni pencere olusturur. 2. Oraya gecis yapar
+        driver.get("http://linkedin.com");
+        Assert.assertTrue(driver.getTitle().contains("LinkedIn"));
+        waitFor(3);
+
+        //        DRIVER NERDE? LINKEDINDE
+        //  TECHPRO SAYFASINA GITMEK ICIN TEKRAR driver.switchTo().window(techProHandle)
+        driver.switchTo().window(techProHandle);
+        waitFor(3);
+
+        // DRIVER NERDE? TECHPRODA
+        Assert.assertTrue(driver.getTitle().contains("Techpro Education"));
+        waitFor(3);
+    }
+
+
 
 }
